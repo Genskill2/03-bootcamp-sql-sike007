407e02e3d0a3b52ee7122cf57087b9d19bdaa431
sike007 <leojolly007@gmail.com>
2021-06-16 14:02:02 +0530

    Soln1

---
+++ create.sql
@@ -0,0 +1,23 @@
+CREATE TABLE publisher (
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	country TEXT
+);
+
+CREATE TABLE books (
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+	publisher INTEGER REFERENCES publisher(id)
+);
+
+CREATE TABLE subjects (
+	id INTEGER PRIMARY KEY,
+	name TEXT	
+);
+
+CREATE TABLE books_subjects (
+	book INTEGER REFERENCES books (id),
+	subject INTEGER REFERENCES subjects(id)
+);
+
+PRAGMA foreign_keys = ON;
---
+++ query1.sql
@@ -0,0 +1,2 @@
+	SELECT books.title FROM books,publisher 
+	WHERE books.publisher=publisher.id AND publisher.name="PHI";
